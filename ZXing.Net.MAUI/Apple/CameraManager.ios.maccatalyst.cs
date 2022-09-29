45e235eed855dcc33b57a4a4e4363b937dd97164
ili <chudinia@gmail.com>
2022-09-29 17:40:27 +0500

    PR 43

--- ZXing.Net.MAUI/Apple/CameraManager.ios.maccatalyst.cs
+++ ZXing.Net.MAUI/Apple/CameraManager.ios.maccatalyst.cs
@@ -13,6 +13,7 @@ using ZXing.Net.Maui.Controls;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Platform;
+using CoreAnimation;
 
 namespace ZXing.Net.Maui;
 
@@ -302,6 +303,25 @@ class PreviewView : UIView
 	public override void LayoutSubviews()
 	{
 		base.LayoutSubviews();
+
+		var transform = CATransform3D.MakeRotation(0, 0, 0, 1.0f);
+		switch (UIDevice.CurrentDevice.Orientation)
+		{
+			case UIDeviceOrientation.Portrait:
+				transform = CATransform3D.MakeRotation(0, 0, 0, 1.0f);
+				break;
+			case UIDeviceOrientation.PortraitUpsideDown:
+				transform = CATransform3D.MakeRotation((nfloat)Math.PI, 0, 0, 1.0f);
+				break;
+			case UIDeviceOrientation.LandscapeLeft:
+				transform = CATransform3D.MakeRotation((nfloat)(-Math.PI / 2), 0, 0, 1.0f);
+				break;
+			case UIDeviceOrientation.LandscapeRight:
+				transform = CATransform3D.MakeRotation((nfloat)Math.PI / 2, 0, 0, 1.0f);
+				break;
+		}
+		PreviewLayer.Transform = transform;
+
 		PreviewLayer.Frame = Layer.Bounds;
 	}
 }
